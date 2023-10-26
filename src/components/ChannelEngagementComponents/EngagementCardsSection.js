20a8353e1c63efb1f309046f30cabe7a19a52eeb
Rahul Sharma <8470314+rahulsharmaah@users.noreply.github.com>
2023-10-27 00:38:59 +0530

    space aded

--- src/components/ChannelEngagementComponents/EngagementCardsSection.js
+++ src/components/ChannelEngagementComponents/EngagementCardsSection.js
@@ -5,7 +5,7 @@ import { Grid } from "@mui/material";
 const EngagementCardsSection = () => {
   return (
     <>
-      <Grid container spacing={8} mt={1}>
+      <Grid container spacing={4} mt={1}>
         {dummyData.map((data, index) => (
           <Grid item xs={12} sm={12} md={4} key={index}>
             <EngagementCard {...data} />
